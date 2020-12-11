11709773da2cb38623030a2d3a4c74e395500ef5
Marta Frąckiewicz <frackiewiczmarta2@gmail.com>
2020-12-11 23:38:20 +0100

    add react router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'normalize.css';
 import Header from "./Header";
 import HotelsList from "./HotelsList";
+import {BrowserRouter, Route} from "react-router-dom";
 
 const App = () => {
     return (
-    <>
-        <Header/>
-        <HotelsList/>
-    </>
+        <BrowserRouter>
+            <>
+                <Header/>
+                <Route exact path='/' component={HotelsList} />
+                {/*<Route path='/admin' component={AdminPanel} />*/}
+            </>
+        </BrowserRouter>
     )
 }
 
