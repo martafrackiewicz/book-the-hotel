3e0a88fc852d6b689b18b372ff78404a3838da0f
Marta Frąckiewicz <frackiewiczmarta2@gmail.com>
2020-12-12 12:52:51 +0100

    add firebase project

--- src/HotelsListElement.js
+++ src/HotelsListElement.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import "./HotelsListElement.css"
+import firebase from "firebase/app";
+
+console.log(firebase)
 
 const HotelsListElement = ({id}) => {
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ import 'normalize.css';
 import Header from "./Header";
 import HotelsList from "./HotelsList";
 import {BrowserRouter, Route} from "react-router-dom";
+import firebase from "firebase/app";
+// import "firebase/auth";
+// import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDLll6YVTKeMsflK2_K2Gzx0-YUwBjQwaA",
+    authDomain: "book-the-hotel-fd8f3.firebaseapp.com",
+    projectId: "book-the-hotel-fd8f3",
+    storageBucket: "book-the-hotel-fd8f3.appspot.com",
+    messagingSenderId: "722436929233",
+    appId: "1:722436929233:web:9bc8777e5a0410739cf7f0"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const App = () => {
     return (
