ae85daf6d057806c8db6f1d1fab052d8fb685e77
Marta Frąckiewicz <frackiewiczmarta2@gmail.com>
2020-12-19 23:43:10 +0100

    display data from firebase

--- src/HotelsList.js
+++ src/HotelsList.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import HotelsListElement from "./HotelsListElement";
+import firebase from "firebase/app";
+import "firebase/firestore";
+// import "firebase/storage";
 
 const HotelsList = () => {
 
-    const hotels = ["hotel1", "hotel2", "hotel3"]
+    const [hotels, setHotels] = useState([]);
+
+    useEffect(() => {
+        const db = firebase.firestore();
+        db.collection('hotels').get()
+            .then(snapshot => {
+            snapshot.docs.forEach(doc => {
+                setHotels(prevState => [...prevState, doc])
+            });
+        })
+    }, []);
+
+
 
     return (
-    <div className={"container"}>
-        <ul className={"hotels_list"}>
-            {hotels.map((el, id) =>
-                <HotelsListElement el={el} key={id}/>)}
-        </ul>
-    </div>
+        <div className={"container"}>
+            <ul className={"hotels_list"}>
+                {hotels.map((el, id) => {
+                    return <HotelsListElement
+                        image={el.data().image_url}
+                        name={el.data().name}
+                        price={el.data().price}
+                        key={id}/>
+                })}
+            </ul>
+        </div>
     )
 }
 
